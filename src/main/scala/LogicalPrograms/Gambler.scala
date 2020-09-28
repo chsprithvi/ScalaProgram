935deee4cbef0226ef3d6546241d9acfdb52abb1
chsprithvi <chsprithvi@gmail.com>
2020-09-28 11:23:23 +0530

    [Siddhartha]Refactored: Changed win percent to float

--- src/main/scala/LogicalPrograms/Gambler.scala
+++ src/main/scala/LogicalPrograms/Gambler.scala
@@ -1,5 +1,32 @@
 package LogicalPrograms
 
-object Gambler {
+import scala.io.StdIn.readLine
+import scala.util.Random
 
+object Gambler {
+  def main(args: Array[String]): Unit = {
+    println("******Welcome To Gambling******")
+    var stake = readLine("Enter Stake Amount:").toInt
+    val goal = readLine("Enter Maximum Amount of the day:").toInt
+    val number = readLine("Enter Number of time you want to play:").toInt
+    var playCount = 0
+    var winCount = 0
+    var lostCount = 0
+    while(stake<=goal && stake>=0 && playCount<number){
+      val checkRandom=Random.nextInt(2)
+      if(checkRandom==0){
+        winCount += 1
+        stake += 1
+      }
+      else{
+        lostCount += 1
+        stake -= 1
+      }
+      playCount += 1
+    }
+    val winPercent= winCount*100/playCount.toFloat
+    val lostPercent= lostCount*100/playCount.toFloat
+    println(s"PlayCount=$playCount,WinCount=$winCount,LostCount=$lostCount")
+    println(s"Win percentage: $winPercent%, Lost Percentage: $lostPercent%")
+  }
 }
