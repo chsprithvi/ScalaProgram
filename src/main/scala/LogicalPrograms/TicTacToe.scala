99356c21f4408bb41af00695f24eabe821fe1f67
chsprithvi <chsprithvi@gmail.com>
2020-10-01 11:33:03 +0530

    [Siddhartha]Added:Display function to display board

--- src/main/scala/LogicalPrograms/TicTacToe.scala
+++ src/main/scala/LogicalPrograms/TicTacToe.scala
@@ -6,14 +6,19 @@ object TicTacToe {
   var arrayIntial=0
   var arrLength: Int =row*column
   var board = new Array[String](arrLength)
-
+ //function to reset board
   def resetBoard(): Unit = {
     for (position <- arrayIntial until arrLength){
       board(position)=" "
     }
   }
+  //function to display
+  def display(): Unit = {
+    println("| " + board(0) + " | " + board(1) + " | " + board(2) + " |\n|-----------|\n| " + board(3) + " | " + board(4) + " | " + board(5) + " |\n|-----------|\n| " + board(6) + " | " + board(7) + " | " + board(8) + " |")
+  }
   def main(args: Array[String]): Unit = {
     println(" %%%%%%% TicTakToe %%%%%%% ")
     resetBoard()
+    display()
   }
 }
